79b8e2307639d7d50bd389aa393cb93e5ad5fc3a
Nick Bury <nmbury@gmail.com>
2013-03-12 12:15:14 -0400

    Add content from json file

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -15,7 +15,28 @@ var main = function () {
         });
     };
 
-    setUpClickHandler($(".tabs .tab"));
+    var JSONLoader = function () {
+        $.getJSON("all.json", function (todos) {
+            todos.forEach(function (todo) {
+                var categoriesString = "";
+                todo.categories.forEach(function (category) {
+                    categoriesString = categoriesString + " " + category;
+                });
+                $("#All").append("<div class='tab active' id='All'>"
+                    + "<p>" + todo.description + "</p>"
+                    + "<p class='categories'>" + categoriesString + "</p>"
+                    + "</div>");
+            });
+        });
+    };
+
+    var initialize = function () {
+        setUpClickHandler($(".tabs .tab"));
+        JSONLoader();
+    };
+
+    initialize();
+
 };
 
 $(document).ready(main);
