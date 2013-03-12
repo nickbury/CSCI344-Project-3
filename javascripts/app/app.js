95e74e546af826f99cb30a7cdafae568e6518c5c
Nick Bury <nmbury@gmail.com>
2013-03-12 11:28:25 -0400

    Add basic tab functionality

---
+++ javascripts/app/app.js
@@ -0,0 +1,21 @@
+/*jslint indent:4*/
+/*global $, document */
+
+var main = function () {
+
+    var setUpClickHandler = function (anchor) {
+        anchor.click(function () {
+            var target = $(this).attr("href");
+
+            $(".active").removeClass("active");
+            $(this).addClass("active");
+            $("#" + target).addClass("active");
+
+            return false;
+        });
+    };
+
+    setUpClickHandler($(".tabs .tab"));
+};
+
+$(document).ready(main);
